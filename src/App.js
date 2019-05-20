714a918ab2eaf18e443bb93e97ea4a43bfce0b74
Fullchee Zhang <fullchee@gmail.com>
2019-05-20 14:38:02 -0400

    Fetch data from hatchways

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const res = await fetch("https://www.hatchways.io/api/assessment/students");
+    if (res.status !== 200) {
+      console.error("Unable to reach API");
+    }
+    const { students } = await res.json();
+    /*
+city: "Fushë-Muhurr"
+company: "Yadel"
+email: "iorton0@imdb.com"
+firstName: "Ingaberg"
+grades: (8) ["78", "100", "92", "86", "89", "88", "91", "87"]
+id: "1"
+lastName: "Orton"
+pic: "https://storage.googleapis.com/hatchways-app.appspot.com/assessments/data/frontend/images/voluptasdictablanditiis.jpg"
+skill: "Oracle"
+    */
+  }
+  render() {
+    return (
+      <div>
+        <p>hi</p>
+      </div>
+    );
+  }
 }
 
 export default App;
