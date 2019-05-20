0aff9a196ced3d71d14d381a26cf9ebcb3e2cf6b
Fullchee Zhang <fullchee@gmail.com>
2019-05-20 17:25:52 -0400

    Add filtering functionality for tags

---
+++ src/components/Filter.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameFilter: "",
+      tagFilter: ""
+    };
+
+    this.searchNameChange = this.searchNameChange.bind(this);
+    this.searchTagChange = this.searchTagChange.bind(this);
+  }
+
+  searchNameChange(event) {
+    this.props.handleFilterChange({
+      nameFilter: event.target.value,
+      tagFilter: this.state.tagFilter
+    });
+    this.setState({ nameFilter: event.target.value });
+  }
+  searchTagChange(event) {
+    this.props.handleFilterChange({
+      nameFilter: this.state.nameFilter,
+      tagFilter: event.target.value
+    });
+    this.setState({ tagFilter: event.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.searchNameChange}
+            placeholder="Search by name"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.searchTagChange}
+            placeholder="Search by tags"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+Filter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired
+};
--- src/components/StudentBox.js
+++ src/components/StudentBox.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Student from "./Student";
+import Filter from "./Filter";
 
 export default class StudentBox extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      filter: "",
       students: [],
       allStudents: []
     };
     this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.addTag = this.addTag.bind(this);
   }
   async componentDidMount() {
     const res = await fetch("https://www.hatchways.io/api/assessment/students");
@@ -18,35 +19,51 @@ export default class StudentBox extends React.Component {
       console.error("Unable to reach API");
     }
     const { students } = await res.json();
-    this.setState({ students: students, allStudents: students });
-  }
-
-  handleFilterChange(event) {
+    const studentsWithTags = students.map(student => {
+      return { ...student, tags: [] };
+    });
     this.setState({
-      value: event.target.value,
-      students: this.state.allStudents.filter(student => {
-        return (
-          student.firstName
-            .toLowerCase()
-            .includes(event.target.value.toLowerCase()) ||
-          student.lastName
-            .toLowerCase()
-            .includes(event.target.value.toLowerCase())
-        );
-      })
+      students: studentsWithTags,
+      allStudents: studentsWithTags
     });
   }
+
+  handleFilterChange({ nameFilter, tagFilter }) {
+    if (nameFilter === "" && tagFilter === "") {
+      this.setState({
+        students: this.state.allStudents
+      });
+    } else {
+      this.setState({
+        students: this.state.allStudents
+          .filter(student => {
+            return (
+              student.firstName.toLowerCase() + student.lastName.toLowerCase()
+            ).includes(nameFilter.toLowerCase());
+          })
+          .filter(student => {
+            return student.tags.some(tag => tag.includes(tagFilter));
+          })
+      });
+    }
+  }
+
+  addTag(index, name) {
+    // prevent duplicate tags
+    this.setState({
+      ...this.state.allStudents,
+      [index]: {
+        tags: this.state.allStudents[index].tags.push(name)
+      }
+    });
+  }
+
   render() {
     return (
       <div id="student-box">
-        <input
-          type="text"
-          value={this.state.value}
-          onChange={this.handleFilterChange}
-          placeholder="Search by name"
-        />
+        <Filter handleFilterChange={this.handleFilterChange} />
         {this.state.students.map(student => (
-          <Student key={student.id} info={student} />
+          <Student key={student.id} info={student} addTag={this.addTag} />
         ))}
       </div>
     );
